68742755f95a707de115c2b64c7b417db6b22460
Lewis Whitear <lewis_whitear@outlook.com>
2020-03-12 19:38:10 +0000

    AJAX and regexes

--- index.php
+++ index.php
@@ -1,5 +1,70 @@
 <html>
 	<!--Front page. The user logs in from here.-->
+	
+	<script type='text/javascript'>
+
+		// Constant strings representing success states that the logon handler uses in its responses
+		const correctCredentials = "CORRECT_CREDENTIALS";
+		const wrongCredentials = "WRONG_CREDENTIALS";
+		const invalidCredentials = "BAD_CREDENTIAL_FORMAT";
+
+		// Submit credentials to logonhandler.php for checking in the DB. Event handler for submit button click.
+		function submitCredentials() {
+
+			// XML HTTP request object.
+			var httpRequest = new XMLHttpRequest();
+
+			// Read in data.
+			var inputUsername = document.getElementById("username").value;
+			var inputPassword = document.getElementById("password").value;
+
+			// Specify the callback function.
+			httpRequest.addEventListener ("load", handleResponse);
+
+			/* 
+				Open the connection to the logon handler.
+				Use GET, this script queries the database.
+			*/
+			httpRequest.open('GET', 'script/logonhandler.php?username=' + inputUsername + '&password=' + inputPassword);
+
+			// Send the request.
+			httpRequest.send();
+		}
+
+		// Callback function for logon requests.
+		function handleResponse(responseData) {
+			// Save the logon handler's response.
+			var responseText = responseData.target.responseText;
+			
+			var displayMessage = "";
+			
+			// Switch on the response message using the constants defined above.
+			switch(responseText) { 
+			
+				case "VALID_CREDENTIAL_FORMAT":
+					displayMessage = "yes";
+					break;
+				case correctCredentials: 
+					break;   
+					
+				case wrongCredentials: 
+					displayMessage = "Incorrect credentials.";
+					break;
+					
+				case invalidCredentials:
+					displayMessage = "Invalid credential format. Are you an evil cracker?";
+					break;
+					
+				default: 
+					break;   
+					
+			}
+			
+			document.getElementById('errormessage').innerHTML = displayMessage;   
+		}
+
+	</script>
+	
 	<head>
 		<title>POI: Points of Interest</title>
 		<link rel="stylesheet" type="text/css" href="style/style.css"/>
@@ -7,34 +72,30 @@
 
 	<body>
 
-		<!--Top bar for logo-->
-		<div class="topbar">
-			<img src="img/poi_logo.png" height=77.5 width=146.5/>
-		</div>
-
 		<!--Centered login form-->
-		<div class="center bordered">
+		<div id="center" class="center bordered">
+			<!--Top bar for logo-->
+			<div class="topbar">
+				<img class="topbarlogo" src="img/poi_logo.png"/>
+			</div>
+		
 			<h1>Points of Interest</h1>
-
-			<!--Querying database. Use GET-->
-			<form action="\script\logonhandler.php" method="get">
-
-				<!--Table to align form fields and labels-->
-				<table class="logonform">
-					<tr>
-						<th><label for="username">Username</label></th>
-						<th><input name="username"></th>
-					</tr>
-					<tr>
-						<th><label for="password">Password</label></th>
-						<th><input type="password" name="password"></th>
-					</tr>
-				</table>
-
-				<input type="submit" value="Log in">
-
-			</form>
+		
+			<!--Table to align form fields and labels-->
+			<table class="logonform">
+				<tr>
+					<th><label for="username">Username</label></th>
+					<th><input id="username"></th>
+				</tr>
+				<tr>
+					<th><label for="password">Password</label></th>
+					<th><input type="password" id="password"></th>
+				</tr>
+			</table>
+			
+			<!-- Call AJAX event handler for button click-->
+			<input type="submit" value="Log in" onclick="submitCredentials()" />
+			<p id="errormessage" class="errorMessage"></p>
 		</div>
-
 	</body>
 </html>
--- script/logonhandler.php
+++ script/logonhandler.php
@@ -1,31 +1,28 @@
 <?php
-     /*
-          This script will validate and check the entered details from the login form
-          If they are correct, they will be redirected to the splash page.
-          The Gatekeeper session variable and the CSRF token are also generated upon successful login.
-     */
+	/*
+	  This script will validate and check the entered details from the login form
+	  If they are correct, they will be redirected to the splash page.
+	  The Gatekeeper session variable and the CSRF token are also generated upon successful login.
+	*/
 
-     /*Regular Expression constants for input validation*/
-     define("USERNAME_REGEX", "");
-     define("PASSWORD_REGEX", "");
+	/*Regular Expression constants for input validation*/
+	define("USERNAME_REGEX", "#([a-z]|[A-Z]){3,15}([0-9]){3}#");
+	define("PASSWORD_REGEX", "#([a-z]|[A-Z]){3,15}([0-9]){3}#");
 
-     /*Get params from query string*/
-     $inputUsername = $_GET["username"];
-     $inputPassword = $_GET["password"];
+	/*Get params from query string*/
+	$inputUsername = $_GET["username"];
+	$inputPassword = $_GET["password"];
 
-     /*
-          Attempt to match the username and password to their regexes defined above
-          Sanitising the input will work to prevent SQL Injection
-     */
-     if (preg_match(USERNAME_REGEX, $inputUsername) && preg_match(PASSWORD_REGEX, $inputPassword)) {
-          /*Credentials are valid, check the database*/
-          echo "haha yes";
-     } else {
-          /*Credentials are invalid, send to a page with an error message*/
-          $errorMsg = "Your details use an invalid format.</br>Are you an evil cracker?";
-
-          /*Use JS to link to the invalid login page, passing the error message in the query string*/
-          echo "<script>location.href='../error_pages/invalid_login.php?errorMsg=$errorMsg';</script>";
-     }
+	/*
+	  Attempt to match the username and password to their regexes defined above
+	  Sanitising the input will work to prevent SQL Injection
+	*/
+	if (preg_match(USERNAME_REGEX, $inputUsername) && preg_match(PASSWORD_REGEX, $inputPassword)) {
+		/*Credentials are valid, check the database*/
+		echo "VALID_CREDENTIAL_FORMAT";
+	} else {
+		/*Credentials are invalid, send an error message back*/
+		echo "BAD_CREDENTIAL_FORMAT";
+	}
 
 ?>
